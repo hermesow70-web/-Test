61cc9c4f93053566b86791026852bfc64b99203b
hermesow70-web <hermesow70@gmail.com>
2026-06-27 04:17:34 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Токены
-BOT_TOKEN = "8645391239:AAGvuHc6CMN3c22g9M-jpNZ4hIGkYrTv_6Q"
-CRYPTO_TOKEN = "601388:AAzYuTp7GYpzv4kQvd9m7JLBNUV3sQ183JR"
-
-# ID админа
-ADMIN_IDS = [595471006]
-
-# Секретный токен для вебхука
-WEBHOOK_SECRET = "KosmosShopSecretKey2026"
-
-# URL для вебхука (заменишь на свой после деплоя на BotHost)
-# Пример: https://kosmos-shop.bothost.net
-WEBHOOK_URL = "https://kosmos-shop.bothost.net/webhook"
-CRYPTO_WEBHOOK_URL = "https://kosmos-shop.bothost.net/crypto-webhook"
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CRYPTO_TOKEN = os.getenv('CRYPTO_TOKEN')
+ADMIN_IDS = [int(os.getenv('ADMIN_IDS'))]
+WEBHOOK_SECRET = os.getenv('WEBHOOK_SECRET')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL', 'https://kosmos-shop.bothost.net/webhook')
